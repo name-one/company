8c9d622d5075ace60ae00b66b970de1a4d9cf768
DenisKamensky <kengji45@gmail.com>
2017-08-23 15:56:22 +0300

    auto scroll on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { nodeForEach } from './common/functions';
 import $ from "jquery";
+import { TimelineMax, TweenMax, Linear } from 'gsap';
+import ScrollMagic from 'scrollmagic';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,27 @@ export class AppComponent implements OnInit {
   title = 'app';
   hiddenMenu: string = 'nav-container_hidden';
   activeScreen = 'home';
+  screenIndex: number;
+  scrollController: any;
+  scrollFlag: boolean = false;
   ngOnInit(){
     this.hideMenu();
+    this.scrollController = new ScrollMagic.Controller();
+    this.detectActiveMobileScreen()
+    if($(window).width() <= 992){
+      this.scrollFlag = true;
+      this.detectActiveMobileScreen();
+    }else{
+      this.scrollFlag = false;
+    }
+    $(window).resize(()=>{
+      if($(window).width() <= 992){
+        this.scrollFlag = true;
+        this.detectActiveMobileScreen();
+      }else{
+        this.scrollFlag = false;
+      }
+    })
   }
   hideMenu(pause?:boolean){
     let time: number = 3000
@@ -29,12 +50,30 @@ export class AppComponent implements OnInit {
     let menu = document.querySelector('.nav-container');
     menu.classList.remove(this.hiddenMenu);
   }
+  changeActiveScreen(index: number){
+    if(index === 3){
+      this.activeScreen = 'about';
+    }else if(index === 2){
+      this.activeScreen = 'projects';
+    }else if(index === 1){
+      this.activeScreen = 'services';
+    }else if(index === 0){
+      this.activeScreen = 'home';
+    }else if(index === 4){
+      this.activeScreen = 'career';
+    }else if(index === 5){
+      this.activeScreen = 'contacts';
+    }
+  }
   toggleScreen(event){
     let target = event.target;
     let screen = $('.screen');
     let wrapper = document.querySelector('.wrapper');
     if(target.tagName == 'A'){
       target = target.parentNode;
+      if($(window).width()<=992){
+        $('body').animate({scrollTop: $('.screen').eq($(target).index()).offset().top})
+      }
     }
     let width = $('.screen').width();
     let menuItems = document.querySelectorAll('.main-menu__item');
@@ -45,34 +84,28 @@ export class AppComponent implements OnInit {
         curIndex = index;
         screen.removeClass('screen_active');
         screen.eq(index).addClass('screen_active');
-        if(index === 3){
-          this.activeScreen = 'about';
-        }else if(index === 2){
-          this.activeScreen = 'projects';
-        }else if(index === 1){
-          this.activeScreen = 'services';
-        }else if(index === 0){
-          this.activeScreen = 'home';
-        }else if(index === 4){
-          this.activeScreen = 'career';
-        }else if(index === 5){
-          this.activeScreen = 'contacts';
-        }
+        this.changeActiveScreen(index);
       }else{
         item.classList.remove('main-menu__item_active');
       }
 
     })
     screen.each( function(){
+      if($(window).width()<= 992){
+        return false
+      }
       if($(this).index() < curIndex){
-        // $(this).animate({ marginLeft: `-${width}px`})
+        console.log('recursion')
         $(this).animate({ marginLeft: `-100vw`})
       }else{
         $(this).animate({ marginLeft: 0 })
       }
     })
-
   }
+
+
+
+
   switchToContactPage(){
     let menuItems = document.getElementsByClassName('main-menu__item');
     let click = new Event('click');
@@ -80,6 +113,32 @@ export class AppComponent implements OnInit {
       if(item.innerHTML.indexOf('Contacts') != -1){
         item.dispatchEvent(click);
       }
+    })
+  }
+  detectActiveMobileScreen( ){
+    if(!this.scrollFlag){
+      console.log('det')
+
+      return false
+    }
+    let self = this;
+    $('.screen').each( function(){
+      new ScrollMagic.Scene({
+        triggerElement: `#${$(this).attr('id')}`
+      })
+      .on('start', (event)=>{
+        if(event.scrollDirection === 'FORWARD' && self.scrollFlag){
+          $('.main-menu__item').eq($(this).index()).click()
+        }
+
+      })
+      .on('leave', (event)=>{
+
+        if(event.scrollDirection === 'REVERSE' && self.scrollFlag){
+            $('.main-menu__item').eq($(this).index()-1).click()
+        }
+      })
+      .addTo(self.scrollController)
     })
   }
 }
