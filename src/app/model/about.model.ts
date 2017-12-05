3984671b9435174f78e42a35b8cbc2adf72ca9d6
DenisKamensky <denis.kamensky@nameone.io>
2017-12-05 20:49:35 +0300

    Improvement (Fixed [About us] several issues). Refs #83.

--- src/app/model/about.model.ts
+++ src/app/model/about.model.ts
@@ -6,17 +6,17 @@ export interface AboutItem{
 export const aboutItems: AboutItem[] = [
   {
     title: 'Our Benefits',
-    description: 'Professional attitude: effective expert solution in required timeline creative thinking: no ordinary approach to existing task greater flexibility: we can listen to your needs and hear them cost efficiency: this is business – yours and ours',
+    description: 'Professional attitude: effective expert solution in required timeline creative thinking: no ordinary approach to existing task greater flexibility: we can listen to your needs and hear them cost efficiency: this is business – yours and ours.',
     icon: 'benefits'
   },
   {
     title: 'Our Mission',
-    description: 'From the very start of our Company we pursue the mission of be coming a trustworthy global professional team in the provision of first-class software development solutions and optimization technologies',
+    description: 'From the very start of our Company we pursue the mission of be coming a trustworthy global professional team in the provision of first-class software development solutions and optimization technologies.',
     icon: 'mission'
   },
   {
     title: 'Our Approach',
-    description: 'Listening, flexibility and professional dialogue is what we believe in. There is nothing in the world impossible to agree upon. Thinking like that allows us to find the best way to deliver effective expert solutions by reducing work time for reasonable price',
+    description: 'Listening, flexibility and professional dialogue is what we believe in. There is nothing in the world impossible to agree upon. Thinking like that allows us to find the best way to deliver effective expert solutions by reducing work time for reasonable price.',
     icon: 'approach'
   },
   {
